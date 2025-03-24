b44d750e297118fddcaaac8ae9e096dc68288546
Felix Kloss <felix.kloss@uni-tuebingen.de>
2025-03-24 11:12:13 +0100

    Add sphinx config for immaterial theme

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,58 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'CompRL'
-copyright = '2025, Carina Straub, Paco Schatz, Paul Masan, Svea Gaenzle, Felix Kloss'
-author = 'Carina Straub, Paco Schatz, Paul Masan, Svea Gaenzle, Felix Kloss'
-release = '1.0.0'
+project = "CompRL"
+copyright = "2025, Universität Tübingen - Autonomous Learning Group"  # noqa: A001
+author = "Carina Straub, Paco Schatz, Paul Masan, Svea Gaenzle, Felix Kloss"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    # "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx_immaterial",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_immaterial"
+
+# Material theme options
+html_theme_options = {
+    # "site_url": "todo",
+    "repo_url": "https://github.com/martius-lab/comprl",
+    "palette": {"primary": "blue-grey", "accent": "orange"},
+    "globaltoc_includehidden": False,
+    "globaltoc_collapse": True,
+    "features": [
+        "navigation.expand",
+        # "navigation.tabs",
+        # "toc.integrate",
+        "navigation.sections",
+        # "navigation.instant",
+        # "header.autohide",
+        "navigation.top",
+        # "navigation.tracking",
+        # "search.highlight",
+        "search.share",
+        "toc.follow",
+        "toc.sticky",
+        "content.tabs.link",
+        # "announce.dismiss",
+    ],
+}
+
+html_static_path = ["_static"]
